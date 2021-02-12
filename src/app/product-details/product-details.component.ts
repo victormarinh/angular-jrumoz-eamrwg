7877b5119e6dd4e55be455e45d85a16e2c798c09
victormarinh <augustovictormarinho@gmail.com>
2021-02-12 16:49:23 -0300

    2 files modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { products } from "../products";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-product-details",
@@ -11,7 +12,15 @@ import { products } from "../products";
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor(private route: ActivatedRoute) {}
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!");
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     // Pegue o primeiro id do produto da rota atual.
